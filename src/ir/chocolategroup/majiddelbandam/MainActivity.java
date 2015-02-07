48264003429f1d26b1792bca9ed5a8aa996e036a
mhsharafi <m.sharfi72@gmail.com>
2015-02-07 11:12:26 +0330

    alaki

--- src/ir/chocolategroup/majiddelbandam/MainActivity.java
+++ src/ir/chocolategroup/majiddelbandam/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends Activity {
 		
 		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
 		
 		setContentView(R.layout.activity_main);
